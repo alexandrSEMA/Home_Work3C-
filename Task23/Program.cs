0b4da6b020423a9c210ac219b4407633bc5d240d
Alexandr <a.semashkev14@gmail.com>
2022-09-10 17:05:54 +0300

    Win

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine(" Введите число ");
+int number = int.Parse(Console.ReadLine()) + 1;
+int[] array = new int[1];
+for (int i = 1; i < number; i++)
+{
+    int[] newArray = array;
+    array = new int[array.Length+1];
+    for (int j = 0; j < newArray.Length; j++)
+    {
+        array [j] = newArray[j];
+    }
+    if (i*i*i %5==0)
+    {
+        array[array.Length] = i * i * i;
+    }
+    System.Console.WriteLine( i * i * i);
+}
+for (int i = 0; i < array.Length ; i++)
+{
+    System.Console.WriteLine(array[i]);
+}
